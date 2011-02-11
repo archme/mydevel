0d06d00c41ef42094c97a50f6e39b6aa30ea70ba
Roman Widmann <roman@ARCDWID01.localdomain>
2011-02-11 09:07:54 +0100

    Added broker and zhelpers

---
+++ zeromq/zhelpers.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+"""
+Helper module for example applications. Mimics ZeroMQ Guide's zhelpers.h.
+"""
+
+from random import randint
+
+import zmq
+
+
+# Receives all message parts from socket, prints neatly
+def dump(zsocket):
+    print "----------------------------------------"
+    for part in zsocket.recv_multipart():
+        print "[%03d]" % len(part),
+        if all(31 < ord(c) < 128 for c in part):
+            print part
+        else:
+            print "".join("%x" % ord(c) for c in part)
+
+
+# Set simple random printable identity on socket
+def set_id(zsocket):
+    identity = "%04x-%04x" % (randint(0, 0x10000), randint(0, 0x10000))
+    zsocket.setsockopt(zmq.IDENTITY, identity)
